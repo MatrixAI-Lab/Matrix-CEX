0020e96800d8571fd639fd85d4ce948c9e5bbfe8
thirteenggh <thirteenggh@gmail.com>
2023-12-12 11:51:46 +0800

    style :

--- config/config.go
+++ config/config.go
@@ -8,14 +8,14 @@ const WsRPC = rpc.DevNet_WS // 开发链
 const CEX_CAPITAL_POOL = "CWub1ia9Bispahk8J3FExFDaPzAk1jYXmAzTiTdxSVLp"
 const CEX_CAPITAL_PK = "2py8uvpGazsSm9zmFSjkx6q37C5fmvTkg2k4hy9H2aaWYiXqs9WTK1aq15W1Y3Dj7vF8KCYMpMQ11dTfQViaxnUC"
 
-const URL = DEV_IP + ":" + DEV_POST
+const URL = DEBUG_IP + ":" + DEBUG_POST
 
 // 本机 develop
 const DEV_IP = "172.18.232.45"
 const DEV_POST = "8080"
 // 3.27.91.240 debug
-const DEBUG_IP = "172.18.232.45"
-const DEBUG_POST = "8080"
+const DEBUG_IP = "0.0.0.0"
+const DEBUG_POST = "8099"
 
 // const DSN = "root:root1234@tcp(localhost:3306)/exchange_db?charset=utf8mb4&parseTime=True&loc=Local"
 const DSN = DEBUG_DB_USER + ":" + DEBUG_DB_PASSWORD + "@tcp(localhost:3306)/" + DB_NAME + "?charset=utf8mb4&parseTime=True&loc=Local"
